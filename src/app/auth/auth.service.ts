449bc1f9ea54844b7ab9e483fde7f43786333cee
Barak Gotesman <barak.gotesman@gmail.com>
2021-09-24 09:50:59 +0300

    changes in auth actions names

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -86,7 +86,7 @@ export class AuthService {
 
         if (loadedUser.token) {
             //this.user.next(loadedUser);
-            this.store.dispatch(new AuthActions.Login({ email: loadedUser.email, userId: loadedUser.id, token: loadedUser.token, expirationDate: new Date(userData._tokenExpirationDate) }));
+            this.store.dispatch(new AuthActions.AuthenticateSuccess({ email: loadedUser.email, userId: loadedUser.id, token: loadedUser.token, expirationDate: new Date(userData._tokenExpirationDate) }));
             const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
             this.autoLogout(expirationDuration);
         }
@@ -127,7 +127,7 @@ export class AuthService {
         );
 
         this.store.dispatch(
-            new AuthActions.Login({
+            new AuthActions.AuthenticateSuccess({
                 email: email,
                 userId: userId,
                 token: token,
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -19,6 +19,13 @@ export interface AuthResponseData {
 
 @Injectable()
 export class AuthEffects {
+
+    @Effect()
+    authSignup = this.actions$.pipe(
+        ofType(AuthActions.SIGNUP_START)
+    );
+
+
     @Effect()
     authLogin = this.actions$.pipe(
         ofType(AuthActions.LOGIN_START),
@@ -35,7 +42,7 @@ export class AuthEffects {
                     map(resData => {
                         const expirationDate = new Date(
                             new Date().getTime() + +resData.expiresIn * 1000);
-                        return new AuthActions.Login({
+                        return new AuthActions.AuthenticateSuccess({
                                 email: resData.email,
                                 userId: resData.localId,
                                 token: resData.idToken,
@@ -45,7 +52,7 @@ export class AuthEffects {
                     catchError(errorRes => {
                         let errorMessage = 'An unknown error occurred!'
                         if (!errorRes.error || !errorRes.error.error)
-                            return of(new AuthActions.LoginFail(errorMessage));
+                            return of(new AuthActions.AuthenticateFail(errorMessage));
                 
                         switch (errorRes.error.error.message) {
                             case 'EMAIL_EXISTS':
@@ -58,7 +65,7 @@ export class AuthEffects {
                                 errorMessage = 'This password is not correct';
                                 break;
                         }
-                        return of(new AuthActions.LoginFail(errorMessage));
+                        return of(new AuthActions.AuthenticateFail(errorMessage));
                     })
                 );
         }),
@@ -67,7 +74,7 @@ export class AuthEffects {
 
     @Effect({dispatch: false})
     authSuccess = this.actions$.pipe(
-        ofType(AuthActions.LOGIN), 
+        ofType(AuthActions.AUTHENTICATE_SUCCESS), 
         tap(() => { 
             this.router.navigate(['/']);
          })
