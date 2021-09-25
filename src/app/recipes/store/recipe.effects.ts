18f4569fec6d7c2564f578a99268125842f99ba6
Barak Gotesman <barak.gotesman@gmail.com>
2021-09-25 12:29:18 +0300

    store in effects

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, withLatestFrom } from 'rxjs/operators';
 import { Recipe } from '../recipe.model';
 import * as RecipesActions from './recipe.actions';
+import * as fromApp from '../../store/app.reducer';
+import { Store } from '@ngrx/store';
 
 @Injectable()
 export class RecipeEffects {
@@ -23,10 +25,26 @@ export class RecipeEffects {
                 };
             });
         }),
-        map(recipes =>{
+        map(recipes => {
             return new RecipesActions.SetRecipes(recipes);
         })
     );
 
-    constructor(private actions$: Actions, private http: HttpClient) { }
+    @Effect({dispatch: false})
+    storeRecipes = this.actions$.pipe(
+        ofType(RecipesActions.STORE_RECIPES),
+        withLatestFrom(this.store.select('recipes')),
+        switchMap(([actionData, recipesState]) => {
+            return this.http.put(
+                'https://recipes-94ec8-default-rtdb.firebaseio.com/recipes.json', 
+            recipesState.recipes
+            );
+        })
+    );
+
+    constructor(
+        private actions$: Actions, 
+        private http: HttpClient, 
+        private store: Store<fromApp.AppState>
+        ) { }
 }
