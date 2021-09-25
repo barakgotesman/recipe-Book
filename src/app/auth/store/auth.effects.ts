83af81e861450b39d7e1c8f6f9e520c7b68622c8
Barak Gotesman <barak.gotesman@gmail.com>
2021-09-25 10:50:10 +0300

    fixing the auth redirect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -29,7 +29,8 @@ const handleAuthentication = (expiresIn: number, email: string, userId: string, 
         email: email,
         userId: userId,
         token: token,
-        expirationDate: expirationDate
+        expirationDate: expirationDate,
+        redirect: true
     });
 };
 
@@ -122,8 +123,11 @@ export class AuthEffects {
     @Effect({ dispatch: false })
     authSuccess = this.actions$.pipe(
         ofType(AuthActions.AUTHENTICATE_SUCCESS),
-        tap(() => {
-            this.router.navigate(['/']);
+        tap((authSuccessAction: AuthActions.AuthenticateSuccess) => {
+            if (authSuccessAction.payload.redirect)
+            {
+                this.router.navigate(['/']);
+            }
         })
     );
 
@@ -158,7 +162,8 @@ export class AuthEffects {
                     email: loadedUser.email,
                     userId: loadedUser.id,
                     token: loadedUser.token,
-                    expirationDate: new Date(userData._tokenExpirationDate)
+                    expirationDate: new Date(userData._tokenExpirationDate),
+                    redirect: false
                 })
 
                 // const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
